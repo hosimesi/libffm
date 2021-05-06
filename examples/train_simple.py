e90bdec5e01afa53fe9541ddb7308405b8361606
c-bata <contact@c-bata.link>
2021-05-06 15:02:12 +0900

    Add regression test

---
+++ examples/train_simple.py
@@ -0,0 +1,24 @@
+import ffm
+
+
+def main():
+    # Prepare the data. (field, index, value) format
+    X = [[(1, 2, 1), (2, 3, 1), (3, 5, 1)],
+         [(1, 0, 1), (2, 3, 1), (3, 7, 1)],
+         [(1, 1, 1), (2, 3, 1), (3, 7, 1), (3, 9, 1)], ]
+    y = [1, 1, 0]
+    train_data = ffm.Dataset(X, y)
+
+    model = ffm.train(
+        train_data,
+        quiet=False
+    )
+    print("Best iteration:", model.best_iteration)
+
+    # Dump FFM weights in ffm-train's "-m" option format.
+    with open("./model/prod-cvr.model", 'w') as f:
+        model.dump_libffm_weights(f)
+
+
+if __name__ == '__main__':
+    main()
