917efe6d05aeff6da4515bc4334da783bcde2a0f
c-bata <contact@c-bata.link>
2021-06-04 14:27:55 +0900

    Refactor regression test

--- regression_test.sh
+++ regression_test.sh
@@ -14,8 +14,8 @@ pyffm-train -p ./bigdata.te.txt -W ./bigdata.iw.txt -f ./model/dummy-2.model -m 
 
 diff ./model/dummy-1.model ./model/dummy-2.model
 
-./ffm-train -p ./bigdata.te.txt -W ./bigdata.iw.txt -m key --auto-stop --auto-stop-threshold 3 ./bigdata.tr.txt ./model/dummy-3.model
-pyffm-train -p ./bigdata.te.txt -W ./bigdata.iw.txt -m key --auto-stop --auto-stop-threshold 3 ./bigdata.tr.txt ./model/dummy-4.model
+./ffm-train -p ./bigdata.te.txt -W ./bigdata.iw.txt --auto-stop --auto-stop-threshold 3 ./bigdata.tr.txt ./model/dummy-3.model
+pyffm-train -p ./bigdata.te.txt -W ./bigdata.iw.txt --auto-stop --auto-stop-threshold 3 ./bigdata.tr.txt ./model/dummy-4.model
 
 diff ./model/dummy-3.model ./model/dummy-4.model
 
